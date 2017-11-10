68895b483c53a094cbc065fa7ba05ba1c92955ec
kiyotaka <kytk>
2017-11-10 12:08:11 +0900

    pos clear

--- Solution/Pacman/Map.cpp
+++ Solution/Pacman/Map.cpp
@@ -59,6 +59,8 @@ void Map::loadStage( std::string stage_name ) {
 		_player_pos[ i ] = Vector( 32 + i * 868, 32 + i * 268 );
 	}
 
+	_feed_pos.clear( );
+	_enemy_pos.clear( );
 	unsigned char object;
 	for ( int i = 0; i < MAP_WIDTH_CHIP_NUM * MAP_HEIGHT_CHIP_NUM; i++ ) {
 		binary->read( (void*)&object, sizeof( unsigned char ) );
